76cb90742c51289d16226ce3b64472483a15afe7
chanCo <loinsective@naver.com>
2022-06-18 00:09:49 +0900

    post

--- express/index.js
+++ express/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// 해당 폴더의 정적인 요소 사용 (이미지 등)
 app.use(express.static('public'));
+// http 요청에서 body 부분을 express가 받아들이게 한다.
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
@@ -15,16 +18,22 @@ app.get('/', (req, res) => {
 //   res.send('안녕하세요!')
 // });
 
-
 app.get('/calculator', (req, res) => {
   let result = Number(req.query.num1) + Number(req.query.num2);
   console.log(req.query);
   res.send(`계산결과 = ${result}`)
 });
 
+// post는 서버로 부터 데이터를 보낼 때 사용
+app.post('/calculator', (req, res) => {
+  console.log(req.body);
+  let result = Number(req.body.num1) + Number(req.body.num2);
+  res.sendFile(__dirname + '/public/result.html');
+});
+
 // 라우팅 맨 마지막에 '*'를 설정하여 404설정 가능(맨위에 하면 안됨)
-app.get('*', (req, res) => {
-  res.status(404).send('찾을 수 없는 페이지입니다.!')
+app.all('*', (req, res) => {
+  res.status(404).send('찾을 수 없는 페이지입니다!')
 });
 
 app.listen(port, () => {
