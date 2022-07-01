f54cea48652f0a16f81efdad2d775dd3b9fe9baa
chanCo <loinsective@naver.com>
2022-07-01 16:43:40 +0900

    connect mongoDB

--- mern/server/index.js
+++ mern/server/index.js
@@ -1,17 +1,27 @@
 import express from 'express';
 import path, { resolve } from 'path';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 4000;
 
-app.use(express.static(path.join(resolve(), '../client/build')));
+// 프로젝트 폴더 위치
+const __dirname = path.resolve();
 
-app.get('/', (req, res, next) => {
-  res.sendFile(path.join(resolve(), '../client/build'));
-});
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  try {
+    mongoose.connect('mongodb+srv://cwoo:1q2w3e@cluster.h1gcy.mongodb.net/?retryWrites=true&w=majority');
+    console.log('Connecting MongoDB!!');
+  } catch(e) {
+    console.error(e);
+  }
   console.log('* * * * * * * * * * * * * * * * * * *');
   console.log('*   E x p r e s s   S e r v e r !   *');
   console.log('* * * * * * * * * * * * * * * * * * *');
+});
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(resolve(), '../client/build'));
 });
